e446e641cd001603b1194d61de5a18a17ae9e9ec
sb06ng <sb06ng01@insightxc.com>
2026-01-15 11:35:38 +0200

    add script 2

---
+++ script2.py
@@ -0,0 +1,27 @@
+class ReverseIterator:
+    def __init__(self, data):
+        # We store the list and start our index at the very end
+        self.data = data
+        self.index = len(data) - 1
+
+    def __iter__(self):
+        # The 'Handshake': An iterator must return itself
+        return self
+
+    def __next__(self):
+        # The 'Step': Check if we've reached the beginning of the list
+        if self.index < 0:
+            raise StopIteration  # The signal to stop the loop
+
+        # Get the item, move the pointer back, and return the item
+        result = self.data[self.index]
+        self.index -= 1
+        return result
+
+
+# --- Usage ---
+my_list = [10, 20, 30, 40]
+rev = ReverseIterator(my_list)
+
+for item in rev:
+    print(item)
